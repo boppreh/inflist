22fa075698ed9618368ad58aae9562a74c63f37d
BoppreH <boppreh@gmail.com>
2013-10-27 21:30:10 -0200

    Add `contains` method

--- inflist.py
+++ inflist.py
@@ -5,6 +5,14 @@ class InfList(object):
     def __getitem__(self, index):
         return self.fn(index)
 
+    def __contains__(self, item):
+        i = 0
+        while True:
+            if item == self.fn(i):
+                return i
+            i += 1
+
 if __name__ == '__main__':
     l = InfList(lambda n: n * 2)
     assert l[10] == 20
+    assert 6 in l
