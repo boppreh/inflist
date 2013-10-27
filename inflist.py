e04c3a21e99d55a722c5e5c230aa821a1078d860
BoppreH <boppreh@gmail.com>
2013-10-27 21:27:26 -0200

    Add simple bracket access support

--- inflist.py
+++ inflist.py
@@ -2,5 +2,9 @@ class InfList(object):
     def __init__(self, fn):
         self.fn = fn
 
+    def __getitem__(self, index):
+        return self.fn(index)
+
 if __name__ == '__main__':
-    assert InfList(None)
+    l = InfList(lambda n: n * 2)
+    assert l[10] == 20
