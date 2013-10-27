02c398022866d7cad70d936c67a988deee4fe7b2
BoppreH <boppreh@gmail.com>
2013-10-27 21:33:49 -0200

    Add iteration support

--- inflist.py
+++ inflist.py
@@ -12,7 +12,19 @@ class InfList(object):
                 return i
             i += 1
 
+    def __iter__(self):
+        i = 0
+        while True:
+            yield self.fn(i)
+            i += 1
+
 if __name__ == '__main__':
     l = InfList(lambda n: n * 2)
     assert l[10] == 20
     assert 6 in l
+
+    for value in l:
+        if value > 7:
+            assert value == 8
+            break
+
